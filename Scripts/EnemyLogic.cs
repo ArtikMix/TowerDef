918438b70d20d0932e0adfe2f5c85800be543b97
ArtikMix <a.katkovski5@gmail.com>
2021-09-29 01:02:15 +0300

    start

---
+++ Scripts/EnemyLogic.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyLogic : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Transform house;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        house = GameObject.FindGameObjectWithTag("house").transform;
+        agent.speed = 2f;
+        agent.stoppingDistance = 0.5f;
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(house.position);
+    }
+}
